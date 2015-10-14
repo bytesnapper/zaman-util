eec5830960e9720b4d0b334f61e451e1c840351d
Ahmed Hosni <ahmedhosni111@gmail.com>
2015-10-14 10:50:22 +0200

    Add AddMonths class to test suite

--- src/test/java/com/bytesnapper/test/DateUtilTest.java
+++ src/test/java/com/bytesnapper/test/DateUtilTest.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({AddDays.class,ResetTime.class})
+@SuiteClasses({AddDays.class,ResetTime.class,AddMonths.class})
 public class DateUtilTest {
 
 }

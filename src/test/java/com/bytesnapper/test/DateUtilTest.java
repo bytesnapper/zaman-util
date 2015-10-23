bd24672f9e246eb2e681be05c3fc057efe0bb7de
Ahmed Hosni <ahmedhosni111@gmail.com>
2015-10-23 18:23:21 +0200

    Add AddIntervals test case

--- src/test/java/com/bytesnapper/test/DateUtilTest.java
+++ src/test/java/com/bytesnapper/test/DateUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({AddDays.class,ResetTime.class,AddMonths.class,AddYears.class,SubtractDate.class,
-	NegativeIntervalWithDate.class,NegativeIntervalWithInterval.class,SubtractIntervals.class
+	NegativeIntervalWithDate.class,NegativeIntervalWithInterval.class,SubtractIntervals.class,AddIntervals.class
 	})
 public class DateUtilTest {
 

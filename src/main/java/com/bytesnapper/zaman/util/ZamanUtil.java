eb437acf2e566049ff143eedf46b2de6a764721b
Ahmed Hosny <ahmedhosni111@gmail.com>
2016-07-29 19:21:39 +0200

    Remove Interval,Fix typos, refactor tests

--- src/main/java/com/bytesnapper/zaman/util/ZamanUtil.java
+++ src/main/java/com/bytesnapper/zaman/util/ZamanUtil.java
@@ -2,12 +2,9 @@ package com.bytesnapper.zaman.util;
 
 import java.util.Calendar;
 import java.util.Date;
-
-import com.bytesnapper.zaman.common.Interval;
-import com.bytesnapper.zaman.exceptions.NegativeIntervalException;
 /**
  * 
- * @author Ahmed Hosni
+ * @author Ahmed Hosny
  *
  */
 public class ZamanUtil {
@@ -23,7 +20,7 @@ public class ZamanUtil {
 	 *            removed from the input date
 	 * @return date after adding the number of days
 	 */
-	public static Date addDaysWithoutResttingTime(Date date, int numberOfDays) {
+	public static Date addDaysWithoutResettingTime(Date date, int numberOfDays) {
 		calendar.setTime(date);
 		calendar.add(Calendar.DATE, numberOfDays);
 		return calendar.getTime();
@@ -77,63 +74,6 @@ public class ZamanUtil {
 
 	/**
 	 * 
-	 * @param fromDate date to start with
-	 * @param toDate   date to end with
-	 * @return an interval 
-	 */
-	public static Interval subtractDate(Date fromDate, Date toDate) {
-		if(fromDate.after(toDate)){
-			throw new NegativeIntervalException("Interval cannot be negative.");
-		}
-		calendar.setTime(fromDate);
-		int fromYears=calendar.get(Calendar.YEAR);
-		int fromMonths=calendar.get(Calendar.MONTH);
-		int fromDays=calendar.get(Calendar.DAY_OF_MONTH);
-		int fromHours=calendar.get(Calendar.HOUR_OF_DAY);
-		int fromMinutes=calendar.get(Calendar.MINUTE);
-		int fromSeconds=calendar.get(Calendar.SECOND);
-		
-		calendar.setTime(toDate);
-		int toYears=calendar.get(Calendar.YEAR);
-		int toMonths=calendar.get(Calendar.MONTH);
-		int toDays=calendar.get(Calendar.DAY_OF_MONTH);
-		int toHours=calendar.get(Calendar.HOUR_OF_DAY);
-		int toMinutes=calendar.get(Calendar.MINUTE);
-		int toSeconds=calendar.get(Calendar.SECOND);
-		
-		return subtractor(fromYears, fromMonths, fromDays, fromHours, fromMinutes, fromSeconds, toYears, toMonths, toDays, toHours, toMinutes, toSeconds);
-	}
-
-	
-	/**
-	 * 
-	 * @param first interval
-	 * @param second interval
-	 * @return interval contains adding result
-	 */
-	public static Interval addIntervals(Interval first, Interval second) {
-		Interval result=adder(first.getYears(), first.getMonths(), first.getDays(),first.getHours(), first.getMinutes(), first.getSeconds(),
-				 second.getYears(), second.getMonths(), second.getDays(),second.getHours(), second.getMinutes(), second.getSeconds());
-		return result;
-}
-
-	/**
-	 * 
-	 * @param first interval
-	 * @param second interval
-	 * @return interval contains subtraction result
-	 */
-	public static Interval subtractIntervals(Interval first, Interval second) {
-		 if(first.greaterThan(second)){
-				throw new NegativeIntervalException("Interval cannot be negative.");
-		 }
-		Interval result=subtractor(first.getYears(), first.getMonths(), first.getDays(),first.getHours(), first.getMinutes(), first.getSeconds(),
-						 second.getYears(), second.getMonths(), second.getDays(),second.getHours(), second.getMinutes(), second.getSeconds());
-		return result;
-	}
-
-	/**
-	 * 
 	 * @return today Date after resetting time
 	 */
 	public static Date getToday() {
@@ -155,128 +95,6 @@ public class ZamanUtil {
 		calendar.set(Calendar.MILLISECOND, calendar.getActualMinimum(Calendar.MILLISECOND));
 		Date outputDate = new Date(calendar.getTimeInMillis());
 		return outputDate;
-	}
-	
-	/**Subtracts members of the interval  
-	 * 
-	 * @param fromYears
-	 * @param fromMonths
-	 * @param fromDays
-	 * @param fromHours
-	 * @param fromMinutes
-	 * @param fromSeconds
-	 * @param toYears
-	 * @param toMonths
-	 * @param toDays
-	 * @param toHours
-	 * @param toMinutes
-	 * @param toSeconds
-	 * @return
-	 */
-	private static Interval subtractor(int fromYears,int fromMonths,int fromDays,int fromHours,int fromMinutes,int fromSeconds,int toYears,int toMonths,int toDays,int toHours,int toMinutes,int toSeconds){
-	
-		if(toSeconds<fromSeconds){
-			toMinutes--;
-			toSeconds+=60;
-		}
-		
-		int outputSeconds=toSeconds-fromSeconds;
-		
-		if(toMinutes<fromMinutes){
-			toHours--;
-			toMinutes+=60;
-		}
-		
-		int outputMinutes=toMinutes-fromMinutes;
-		
-		if(toHours<fromHours){
-			toDays--;
-			toHours+=24;
-		}
-		
-		int outputHours=toHours-fromHours;
-		
-
-		if(toDays<fromDays){
-			toMonths--;
-			toDays+=30;
-		}
-		
-		int outputDays=toDays-fromDays;
-		
-		if(toMonths<fromMonths){
-			toYears--;
-			toMonths+=12;
-		}
-		
-		int outputMonths=toMonths-fromMonths;
-		
-		
-			int outputYears=toYears-fromYears;
-			Interval interval = new Interval();
-			interval.setSeconds(outputSeconds);
-			interval.setMinutes(outputMinutes);
-			interval.setHours(outputHours);
-			interval.setDays(outputDays);
-			interval.setMonths(outputMonths);
-			interval.setYears(outputYears);
-
-
-			return interval;
-	
-	}
-	
-	
-	private static Interval adder(int fromYears,int fromMonths,int fromDays,int fromHours,int fromMinutes,int fromSeconds,int toYears,int toMonths,int toDays,int toHours,int toMinutes,int toSeconds){
-		
-		int outputSeconds=toSeconds+fromSeconds;
-
-		if(outputSeconds>59){
-			toMinutes++;
-			outputSeconds-=60;
-		}
-		
-		int outputMinutes=toMinutes+fromMinutes;
-
-		
-		if(outputMinutes>59){
-			toHours++;
-			outputMinutes-=60;
-		}
-		
-		int outputHours=toHours+fromHours;
-
-		if(outputHours>23){
-			toDays++;
-			outputHours-=24;
-		}
-		
-		int outputDays=toDays+fromDays;
-
-
-		if(outputDays>29){
-			toMonths++;
-			outputDays-=30;
-		}
-		
-		int outputMonths=toMonths+fromMonths;
-
-		if(outputMonths>11){
-			toYears++;
-			outputMonths-=12;
-		}
-		
-		int outputYears=toYears+fromYears;
-		Interval interval = new Interval();
-		interval.setSeconds(outputSeconds);
-		interval.setMinutes(outputMinutes);
-		interval.setHours(outputHours);
-		interval.setDays(outputDays);
-		interval.setMonths(outputMonths);
-		interval.setYears(outputYears);
-		return interval;
-		
-		
 	}
 	
 	
--- src/test/java/com/bytesnapper/test/ZamanUtilTest.java
+++ src/test/java/com/bytesnapper/test/ZamanUtilTest.java
@@ -1,13 +1,155 @@
 package com.bytesnapper.test;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import static org.junit.Assert.fail;
 
-@RunWith(Suite.class)
-@SuiteClasses({AddDays.class,ResetTime.class,AddMonths.class,AddYears.class,SubtractDate.class,
-	NegativeIntervalWithDate.class,NegativeIntervalWithInterval.class,SubtractIntervals.class,AddIntervals.class
-	})
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bytesnapper.zaman.util.ZamanUtil;
+
 public class ZamanUtilTest {
+
+	@Test
+	public void testAddDays() {
+		int addition = 10;
+		Date inputDate = new Date();
+		Date outputDate = ZamanUtil.addDays(inputDate, addition);
+
+		Assert.assertFalse("Date is null", outputDate == null);
+		Assert.assertFalse("Number of Days is not added ", addition > 0 && !outputDate.after(inputDate));
+
+		addition = -1;
+		inputDate = new Date();
+		outputDate = ZamanUtil.addDays(inputDate, addition);
+
+		Assert.assertFalse("Number of Days is not subtracted", addition < 0 && !outputDate.before(inputDate));
+
+		addition = 0;
+		inputDate = new Date();
+		outputDate = ZamanUtil.addDaysWithoutResettingTime(inputDate, addition);
+
+		Assert.assertFalse("Number of Days is 0 , it should not change input date",
+				addition == 0 && !inputDate.equals(outputDate));
+
+	}
+
+	@Test
+	public void testAddMonths() {
+		Calendar calendar = Calendar.getInstance();
+		int months = 10;
+		int inMonth = calendar.get(Calendar.MONTH);
+		int inYear = calendar.get(Calendar.YEAR);
+
+		Date outputDate = ZamanUtil.addMonths(calendar.getTime(), months);
+		calendar.setTime(outputDate);
+		int outMonth = calendar.get(Calendar.MONTH);
+		int outYear = calendar.get(Calendar.YEAR);
+
+		Assert.assertFalse("Months param  greater than 0 and output month is not greater than input month", (months > 0
+				&& inMonth > outMonth && !(outYear > inYear)));
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		months = -2;
+		inMonth = calendar.get(Calendar.MONTH);
+		inYear = calendar.get(Calendar.YEAR);
+		outputDate = ZamanUtil.addMonths(calendar.getTime(), months);
+		calendar.setTime(outputDate);
+		outMonth = calendar.get(Calendar.MONTH);
+		outYear = calendar.get(Calendar.YEAR);
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		Assert.assertFalse("Months param is less than 0 and output month is not less than input month", months < 0
+				&& inMonth < outMonth && !(outYear < inYear));
+
+		months = 0;
+		inMonth = calendar.get(Calendar.MONTH);
+		inYear = calendar.get(Calendar.YEAR);
+
+		outputDate = ZamanUtil.addMonths(calendar.getTime(), months);
+		calendar.setTime(outputDate);
+		outMonth = calendar.get(Calendar.MONTH);
+		outYear = calendar.get(Calendar.YEAR);
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		Assert.assertFalse("Months param is equal to 0 and output month is not greater than input month", (months == 0
+				&& inMonth != outMonth && outYear != inYear));
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+	}
+
+	@Test
+	public void addYearsTest() {
+		Calendar calendar = Calendar.getInstance();
+		int years = 10;
+		int inMonth = calendar.get(Calendar.MONTH);
+		int inYear = calendar.get(Calendar.YEAR);
+
+		Date outputDate = ZamanUtil.addYears(calendar.getTime(), years);
+		calendar.setTime(outputDate);
+		int outMonth = calendar.get(Calendar.MONTH);
+		int outYear = calendar.get(Calendar.YEAR);
+
+		Assert.assertFalse("Years param  greater than 0 and output year is not greater than input year",
+				(years > 0 && !(outYear > inYear)));
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		years = -2;
+		inMonth = calendar.get(Calendar.MONTH);
+		inYear = calendar.get(Calendar.YEAR);
+		outputDate = ZamanUtil.addYears(calendar.getTime(), years);
+		calendar.setTime(outputDate);
+		outMonth = calendar.get(Calendar.MONTH);
+		outYear = calendar.get(Calendar.YEAR);
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		Assert.assertFalse("Years param is less than 0 and output year is not less than input year",
+				(years < 0 && !(outYear < inYear)));
+
+		years = 0;
+		inMonth = calendar.get(Calendar.MONTH);
+		inYear = calendar.get(Calendar.YEAR);
+
+		outputDate = ZamanUtil.addYears(calendar.getTime(), years);
+		calendar.setTime(outputDate);
+		outMonth = calendar.get(Calendar.MONTH);
+		outYear = calendar.get(Calendar.YEAR);
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+		Assert.assertFalse("Years param is equal to 0 and output year is not greater than input year", (years == 0
+				&& inMonth != outMonth && outYear != inYear));
+
+		System.out.println("Input Date: " + inMonth + " " + outYear);
+		System.out.println("Output Date: " + outMonth + " " + outYear);
+
+	}
+	
+	@Test
+	public void testResetTime() {
+		Date inputDate= new Date();
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+		Date outputDate=ZamanUtil.resetTime(inputDate);
+		String outputDateStr= format.format(outputDate);
+		if(!outputDateStr.equals("00:00:00")){
+			fail("Time is not reset");
+		}
+	}
 
 }
